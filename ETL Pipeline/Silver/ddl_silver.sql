cda3c1df05d14d5895825f42535f576eb9f534a3
Ain Nadhirah <107626922+ainnadhiraah@users.noreply.github.com>
2026-04-06 15:38:23 +0800

    Update ddl_silver.sql

--- ETL Pipeline/Silver/ddl_silver.sql
+++ ETL Pipeline/Silver/ddl_silver.sql
@@ -6,8 +6,6 @@ Script Purpose:
     This script creates tables in the 'silver' schema, dropping existing tables 
     if they already exist.
 	  Run this script to re-define the DDL structure of 'silver' Tables
-      
-      
 ===============================================================================
 */
 
